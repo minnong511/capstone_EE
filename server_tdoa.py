137ed65e7b3a00441c85af1a21778376039228b6
minnong511 <kzxx1234@seoultech.ac.kr>
2025-09-17 15:27:14 +0900

    Remove tracked files so .gitignore applies

--- server_tdoa.py
+++
@@ -1,7 +0,0 @@
-# server.py
-from node.node_wifi import run_listener_for_all
-
-if __name__ == "__main__":
-    # 같은 폴더(경로)로 맞춰야 워커가 파일을 읽음
-    run_listener_for_all(real_time_folder="./Input_data/real_input_tdoa",
-                         host="0.0.0.0", port=5050)
